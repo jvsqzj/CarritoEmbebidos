b156e56c9af92b6093b0ef934d10e8d6b242c9e3
jvsqzj <jvsqzj@gmail.com>
2018-05-25 15:52:07 -0600

    Bot worker

--- bot.js
+++ bot.js
@@ -45,5 +45,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
  
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, ip);
+  bot.sendMessage(chatId, ip + ":" + port);
 });
