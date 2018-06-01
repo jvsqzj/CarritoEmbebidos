0d5a48667fc6593f998b8c739501b527f247b2e1
gabrielrodriguezrivera <gabo1294@gmail.com>
2018-06-01 15:10:21 -0600

    prueba sensor

--- sensor.js
+++ sensor.js
@@ -1,21 +1,30 @@
-var tessel = require('tessel');
-var gpio = tessel.port.GPIO;
-// The trigger pin can be any GPIO
-var triggerPin = gpio.digital[1];
-// The echo pin MUST be G3 on the GPIO bank
-var echoPin = gpio.digital[2];
+var Gpio = require('pigpio').Gpio,
+  trigger = new Gpio(23, {mode: Gpio.OUTPUT}),
+  echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
-var proxLib = require('../');
-var proximity = proxLib.use(triggerPin, echoPin);
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+var MICROSECDONDS_PER_CM = 1e6/34321;
 
-function printDistance() {
-  proximity.getDistance(function(err, distance) {
-    if (err) throw err;
+trigger.digitalWrite(0); // Make sure trigger is low
 
-    console.log("Distance: ", distance, "cm away.");
+(function () {
+  var startTick;
 
-    printDistance();
+  echo.on('alert', function (level, tick) {
+    var endTick,
+      diff;
+
+    if (level == 1) {
+      startTick = tick;
+    } else {
+      endTick = tick;
+      diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+      console.log(diff / 2 / MICROSECDONDS_PER_CM);
+    }
   });
-}
+}());
 
-printDistance();
+// Trigger a distance measurement once per second
+setInterval(function () {
+  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+}, 1000);
